aa95f1dfdb3dd2b3b467902fe40253396065f217
김동건 <wrjssmjdhappy@gmail.com>
2020-10-01 13:40:34 +0900

    Hello World

---
+++ HelloWorld/app/src/main/java/kr/ac/hs/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package kr.ac.hs.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ex1);
+    }
+}
